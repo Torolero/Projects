f2a31c6ac6118e06600e119c22ef58b24faf823f
Torolero <os106@icloud.com>
2024-05-17 10:50:03 -0700

    Pushing all files in dir.

--- Practice/assignment11.c
+++ Practice/assignment11.c
@@ -6,12 +6,11 @@ int main(void)
 
     char decision;
     float average;
-    //printf("Enter a test score:");
-    //scanf("%i", &testScores[0]);
-
+    // printf("Enter a test score:");
+    // scanf("%i", &testScores[0]);
 
     for (int i = 0; i <= 7; i++)
-    
+
     {
         printf("Would you like to continue? Y/N: ");
         scanf("%s", &decision);
@@ -23,7 +22,7 @@ int main(void)
         }
         else if (decision == 'n' || decision == 'N')
         {
-            average = ((testScores[0] + testScores[1] +testScores[2] +testScores[3] +testScores[4] +testScores[5] +testScores[6] +testScores[7]) / (i));
+            average = ((testScores[0] + testScores[1] + testScores[2] + testScores[3] + testScores[4] + testScores[5] + testScores[6] + testScores[7]) / (i));
             break;
         }
     }
