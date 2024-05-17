86e1999016fbd8b66b6dde3ab7497517bf919699
Torolero <os106@icloud.com>
2024-05-17 15:59:30 -0700

    Saving this.

--- Practice/assignment12.c
+++ Practice/assignment12.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -6,5 +7,6 @@ int main(void)
     printf("How many times would you like to flip the coin? ");
     scanf("%i", &coinFlips);
 
-    printf("This number: %i", coinFlips);
+    int rand(void);
+https: // www.tutorialspoint.com/rand-and-srand-in-c
 }
