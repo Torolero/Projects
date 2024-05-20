06b7c44447f355000ea2a76ed90bfed5124ec555
Torolero <os106@icloud.com>
2024-05-20 16:11:53 -0700

    This is to commit practice i've been doing with Srand.

--- Practice/assignment12.c
+++ Practice/assignment12.c
@@ -4,9 +4,15 @@
 int main(void)
 {
     int coinFlips;
+
     printf("How many times would you like to flip the coin? ");
     scanf("%i", &coinFlips);
 
+    void srand(unsigned int coinFlips);
+
+    int heads = rand() % coinFlips;
+    int tails = rand() % coinFlips;
+
     int rand(void);
-https: // www.tutorialspoint.com/rand-and-srand-in-c
+    printf("After flipping the coin %i times, the results were\n %i heads\n %i tails\n", coinFlips, heads, tails);
 }
