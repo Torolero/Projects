dc854e849c2681ff83de72c1838829e18ba8688d
Torolero <os106@icloud.com>
2024-05-20 20:04:04 -0700

    In this commit I have made progress in #13 by working with 2d array.

--- Practice/assignment13.c
+++ Practice/assignment13.c
@@ -4,15 +4,21 @@ int main(void)
 {
     int p1, p2, p3, p4, p5;
     int G1, G2, G3, G4;
-
+    int players[4][5] = {{}, {}};
     // Outter for loop will define on a per game basis.
-    for (size_t i = 0; i < count; i++)
+    for (int i = 0; i < 4; i++)
     {
+        printf("Game # \n");
+
+        printf("Enter scoring total for Player # :");
+        scanf("%i", &players);
+
         // Inner for loop will go through the players
-        for (size_t i = 0; i < count; i++)
+        for (int j = 0; j < 5; j++)
         {
             /* code */
         }
     }
+    printf("Player #XXX had the highest scoring average at XXX points per game.\n");
     // Create arrays that will store score totals and calculate the averages
 }
