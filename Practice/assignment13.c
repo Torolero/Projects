7f5b2d136d00dc9d370f4966290a34edc32c38c0
Torolero <os106@icloud.com>
2024-05-20 18:53:25 -0700

    This is to push to my pc from work pc.

---
+++ Practice/assignment13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int p1, p2, p3, p4, p5;
+    int G1, G2, G3, G4;
+
+    // Outter for loop will define on a per game basis.
+    for (size_t i = 0; i < count; i++)
+    {
+        // Inner for loop will go through the players
+        for (size_t i = 0; i < count; i++)
+        {
+            /* code */
+        }
+    }
+    // Create arrays that will store score totals and calculate the averages
+}
