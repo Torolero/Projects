44f4ab5fee23415170c64d8ab838a5cfc8b62492
Torolero <os106@icloud.com>
2024-05-20 20:20:56 -0700

    Already created loop for number of games. Need to work on loop for players next.

--- Practice/assignment13.c
+++ Practice/assignment13.c
@@ -2,21 +2,21 @@
 
 int main(void)
 {
-    int p1, p2, p3, p4, p5;
-    int G1, G2, G3, G4;
     int players[4][5] = {{}, {}};
+
     // Outter for loop will define on a per game basis.
     for (int i = 0; i < 4; i++)
     {
-        printf("Game # \n");
-
-        printf("Enter scoring total for Player # :");
-        scanf("%i", &players);
+        int games = i; 
+        printf("Game #%i\n", games + 1);
 
         // Inner for loop will go through the players
         for (int j = 0; j < 5; j++)
         {
-            /* code */
+
+        //printf("Enter scoring total for Player # :");
+        //scanf("%i", &players);
+
         }
     }
     printf("Player #XXX had the highest scoring average at XXX points per game.\n");
