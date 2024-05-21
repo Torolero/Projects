cabf473cdecdcf3166432f54b9d370860a0d7e1e
Torolero <os106@icloud.com>
2024-05-20 19:50:58 -0700

    Blank commit.

--- Practice/assignment12.c
+++ Practice/assignment12.c
@@ -13,7 +13,7 @@ int main(void)
 
     int heads = rand() % coinFlips;
     int tails = coinFlips - heads;
-    //The program works until here, it will give you two random values between 0 and your input. Now I need to figure out how to make them equal to user input when added together.
+    // The program works until here, it will give you two random values between 0 and your input. Now I need to figure out how to make them equal to user input when added together.
 
     printf("After flipping the coin %i times, the results were\n %i heads\n %i tails\n", coinFlips, heads, tails);
 }
