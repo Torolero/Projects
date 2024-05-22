3b3123462f8d2f7b76682762181d76605b08dcad
Torolero <os106@icloud.com>
2024-05-21 22:12:00 -0700

    Assignment 22 on nested array of structures.

--- Practice/assignment22.c
+++ Practice/assignment22.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 
-struct stats {
-  int points;
-  int games;
+struct stats
+{
+    int points;
+    int games;
 };
-
-struct stats player[5];
 
 int main(void)
 {
+    struct stats player[5];
     struct stats players;
 
     for (int i = 0; i < 5; i++)
     {
-        printf("Enter Player %i's point total:", i +1);
+        //player[i].games = i;
+        printf("Enter Player %i's point total:", i + 1);
         scanf("%i", &players.points);
 
-        printf("Enter Player %i's game total:", i +1);
+        printf("Enter Player %i's game total:", i + 1);
         scanf("%i", &players.games);
     }
-    
 
+    /*for (int j = 0; j < 5; j++)
+    {
+        int average = (player[j].points) / (5);
+        printf("%i", average);
+    }*/
+
+    int player1 = (player[0].points) / (5);
+    printf("%i", player1);
+
+    /*int player2 = (player[1].points) / (players.games);
+    printf("%i", player2);
+
+    int player3 = (player[2].points) / (players.games);
+    printf("%i", player3);
+
+    int player4 = (player[3].points) / (players.games);
+    printf("%i", player4);
+
+    int player5 = (player[0].points) / (players.games);
+    printf("%i", player5);*/
 }
